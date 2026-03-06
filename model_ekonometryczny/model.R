bf95c92da865b79a9ceab46ff92b3ac7e9f8d836
StaDra324 <s.drag@student.uw.edu.pl>
2026-03-06 15:37:30 +0100

    Czyszczenie kodów 1
    Modele zaakceptowane, teraz kody są do wyczyszczenia. Na razie wyczyszczony został kod do modelu model.R. Kwestia czy coś się zmieni, bo Lyft jest do usunięcia więc zbiór będzie lekko inny

--- model_ekonometryczny/model.R
+++ model_ekonometryczny/model.R
@@ -1,17 +1,4 @@
-# - regresję: cena – determinanty,  
-# - regresję: log(cena) – determinanty,  
-# - regresję: log(cena) – log(determinanty) w przypadku zmiennych ciągłych,  
-# - sprawdzić macierz korelacji.
-# 
-# Następnie proszę rozpocząć diagnostykę modelu (analiza reszt itp.).
-# 
-# RESET:
-#   
-#   - dodać dystans² oraz czas²,  
-# - dodać sensowne interakcje (np. dystans × czas),  
-# - dodać zmienne dodatkowe, jeżeli coś można wykorzystać z bazy.
-# - jeżeli było na ekonometrii transformacja Boxa-Coxa, można zastosować 
-
+library("readr")
 library("car")
 library("lmtest")
 library("foreign")
@@ -20,23 +7,14 @@ library("dplyr")
 library("ggplot2")
 library("sandwich")
 library("tseries")
-library("MASS")
 library("lubridate")
 
 # WCZYTANIE I DOSTOSOWANIE DANYCH
-lyft = read.csv("lyft_do_modelu.csv")
-uber = read.csv("uber_do_modelu.csv")
-taxi = read.csv("taxi_do_modelu.csv")
+uber = read_csv("uber_do_modelu.csv")
+taxi = read_csv("taxi_do_modelu.csv")
 
-# Uwzględnienie 01.01 (Nowy rok)
+# Uwzględnienie 01.01 (Nowy Rok), 19.01 i 16.02
 
-class(uber$datetime)
-# można zmienic zeby to co wyzej szybciej dzialalo
-
-lyft <- lyft %>%
-  mutate(czy_01_01 = if_else(month(datetime) == 1 & day(datetime) == 1, 1, 0),
-         czy_19_01 = if_else(month(datetime) == 1 & day(datetime) == 19, 1, 0),
-         czy_16_02 = if_else(month(datetime) == 2 & day(datetime) == 16, 1, 0))
 uber <- uber %>%
   mutate(czy_01_01 = if_else(month(datetime) == 1 & day(datetime) == 1, 1, 0),
          czy_19_01 = if_else(month(datetime) == 1 & day(datetime) == 19, 1, 0),
@@ -46,133 +24,59 @@ taxi <- taxi %>%
          czy_19_01 = if_else(month(datetime) == 1 & day(datetime) == 19, 1, 0),
          czy_16_02 = if_else(month(datetime) == 2 & day(datetime) == 16, 1, 0))
 
+
+# Dodanie grup godzin i dni (pór dnia i weekendów)
+
+uber <- uber %>%
+  mutate(
+    hour_gr = case_when(
+      hour %in% 7:8              ~ "7-9",
+      hour %in% c(6, 9:16)       ~ "6, 9-17",
+      hour %in% 17:20            ~ "17-21",
+      hour %in% 21:22            ~ "21-23",      
+      hour %in% c(23, 0:5)       ~ "23-6",
+      TRUE                       ~ NA_character_))
+
+taxi <- taxi %>%
+  mutate(
+    hour_gr = case_when(
+      hour %in% 7:8              ~ "7-9",
+      hour %in% c(6, 9:16)       ~ "6, 9-17",
+      hour %in% 17:20            ~ "17-21",
+      hour %in% 21:22            ~ "21-23",      
+      hour %in% c(23, 0:5)       ~ "23-6",
+      TRUE                       ~ NA_character_))
+
+# Upewniamy się, że nie ma NA
+anyNA(uber) | anyNA(taxi)
+
 # Ustawienie zmiennych kategorycznych jako factorów i określenie baz
-class(uber$hour)
+
 class(uber$czy_weekend)
 class(uber$month)
+class(uber$hour_gr)
 
-lyft$hour <- relevel(as.factor(lyft$hour), ref = "12")
-uber$hour <- relevel(as.factor(uber$hour), ref = "12")
-taxi$hour <- relevel(as.factor(taxi$hour), ref = "12")
-lyft$czy_weekend <- relevel(as.factor(lyft$czy_weekend), ref = "Robocze")
 uber$czy_weekend <- relevel(as.factor(uber$czy_weekend), ref = "Robocze")
 taxi$czy_weekend <- relevel(as.factor(taxi$czy_weekend), ref = "Robocze")
-lyft$month <- relevel(as.factor(lyft$month), ref = "1")
+
 uber$month <- relevel(as.factor(uber$month), ref = "1")
 taxi$month <- relevel(as.factor(taxi$month), ref = "1")
 
+uber$hour_gr <- relevel(as.factor(uber$hour_gr), ref = "6, 9-17")
+taxi$hour_gr <- relevel(as.factor(taxi$hour_gr), ref = "6, 9-17")
+
+class(uber$czy_weekend)
+class(uber$month)
+class(uber$hour_gr)
+
+# Jeszcze raz sprawdzamy NA
+anyNA(uber) | anyNA(taxi)
 
 # REGRESJE total_amount
 
 # Log - lin
-lyft_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-                      hour + 
-                      czy_weekend +
-                      hour:czy_weekend +
-                      month +
-                      czy_01_01 +
-                      czy_19_01 +
-                      czy_16_02,
-                    data = lyft)
-resettest(lyft_m, power = 2:3, type = "fitted")
 
 uber_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-                      hour + 
-                      czy_weekend +
-                      hour:czy_weekend +
-                      month +
-                      czy_01_01 +
-                      czy_19_01 +
-                      czy_16_02,
-                    data = uber)
-resettest(uber_m, power = 2:3, type = "fitted")
-
-taxi_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-                      hour + 
-                      czy_weekend +
-                      hour:czy_weekend +
-                      month +
-                      czy_01_01 +
-                      czy_19_01 +
-                      czy_16_02,
-                    data = taxi)
-resettest(taxi_m, power = 2:3, type = "fitted")
-
-stargazer(lyft_m, uber_m, taxi_m,
-          type = "text", align = TRUE, style = "default", df = FALSE)
-
-
-
-# DODANIE GRUP GODZIN I DNI
-
-lyft <- lyft %>%
-  mutate(
-    hour_gr = case_when(
-      hour %in% 7:8              ~ "7-9",
-      hour %in% c(6, 9:16)       ~ "6, 9-17",
-      hour %in% 17:20            ~ "17-21",
-      hour %in% 21:22            ~ "21-23",      
-      hour %in% c(20:23, 0:5)    ~ "23-6",
-      TRUE                       ~ NA_character_))
-
-uber <- uber %>%
-  mutate(
-    hour_gr = case_when(
-      hour %in% 7:8              ~ "7-9",
-      hour %in% c(6, 9:16)       ~ "6, 9-17",
-      hour %in% 17:20            ~ "17-21",
-      hour %in% 21:22            ~ "21-23",      
-      hour %in% c(20:23, 0:5)    ~ "23-6",
-      TRUE                       ~ NA_character_))
-
-taxi <- taxi %>%
-  mutate(
-    hour_gr = case_when(
-      hour %in% 7:8              ~ "7-9",
-      hour %in% c(6, 9:16)       ~ "6, 9-17",
-      hour %in% 17:20            ~ "17-21",
-      hour %in% 21:22            ~ "21-23",      
-      hour %in% c(20:23, 0:5)    ~ "23-6",
-      TRUE                       ~ NA_character_))
-
-lyft$hour_gr <- relevel(as.factor(lyft$hour_gr), ref = "6, 9-17")
-uber$hour_gr <- relevel(as.factor(uber$hour_gr), ref = "6, 9-17")
-taxi$hour_gr <- relevel(as.factor(taxi$hour_gr), ref = "6, 9-17")
-
-
-
-# DODANIE LICZBY PRZEJAZDÓW W KONKRETNYCH GODZINACH
-
-lyft <- lyft %>%
-  group_by(date = as.Date(datetime), time_20min = round_date(ymd_hms(datetime), "20 minutes")) %>%
-  mutate(ride_count = n()) %>%
-  ungroup()
-
-uber <- uber %>%
-  group_by(date = as.Date(datetime), time_20min = round_date(ymd_hms(datetime), "20 minutes")) %>%
-  mutate(ride_count = n()) %>%
-  ungroup()                   
-
-taxi <- taxi %>%
-  group_by(date = as.Date(datetime), time_20min = round_date(ymd_hms(datetime), "20 minutes")) %>%
-  mutate(ride_count = n()) %>%
-  ungroup()                  
-
-# Log - lin
-lyft_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-               ride_count +
-               hour_gr + 
-               czy_weekend +
-               hour_gr:czy_weekend +
-               month +
-               czy_01_01 +
-               czy_19_01 +
-               czy_16_02,
-             data = lyft)
-resettest(lyft_m, power = 2:3, type = "fitted")
-
-uber_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-               ride_count +
                hour_gr + 
                czy_weekend +
                hour_gr:czy_weekend +
@@ -182,9 +86,9 @@ uber_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
                czy_16_02,
              data = uber)
 resettest(uber_m, power = 2:3, type = "fitted")
+# RESET nie przechodzi
 
 taxi_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-               ride_count +
                hour_gr + 
                czy_weekend +
                hour_gr:czy_weekend +
@@ -194,50 +98,33 @@ taxi_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
                czy_16_02,
              data = taxi)
 resettest(taxi_m, power = 2:3, type = "fitted")
+# RESET nie przechodzi
 
-stargazer(lyft_m, uber_m, taxi_m,
+stargazer(uber_m, taxi_m,
           type = "text", align = TRUE, style = "default", df = FALSE)
 
-# MACIERZ KORELACJI
+# Macierz korelacji
 
-cor(lyft_marzec[, c("total_amount", "distance", "trip_time_mins")],
+cor(uber[, c("total_amount", "distance", "trip_time_mins")],
     method = "spearman")
-cor(uber_marzec[, c("total_amount", "distance", "trip_time_mins")],
-    method = "spearman")
-cor(taxi_marzec[, c("total_amount", "distance", "trip_time_mins")],
+cor(taxi[, c("total_amount", "distance", "trip_time_mins")],
     method = "spearman")
 
+# Współczynnik VIF
 
-# VIF
-
-vif(lyft_m, type = "predictor")
 vif(uber_m, type = "predictor")
 vif(taxi_m, type = "predictor")
-# dla log - log distance i trip_time dużo, dla log - lin ok
+# wychodzi ok
 
 
 # DIAGNOSTYKA
 
-# Dodanie kwadratów i interakcji - RESET  
-  
-lyft_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-                 I(distance**2) +
-                 I(trip_time_mins**2) +
-                 ride_count +
-                 hour_gr + 
-                 czy_weekend +
-                 hour_gr:czy_weekend +
-                 month +
-                 czy_01_01 +
-                 czy_19_01 +
-                 czy_16_02,
-               data = lyft)
-resettest(lyft_m, power = 2:3, type = "fitted")
+# Dodanie kwadratów i interakcji distance i trip_time_mins - RESET  
 
-uber_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
+uber_m2 <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
                I(distance**2) +
                I(trip_time_mins**2) +
-               ride_count +
+               distance:trip_time_mins +
                hour_gr + 
                czy_weekend +
                hour_gr:czy_weekend +
@@ -246,12 +133,13 @@ uber_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
                czy_19_01 +
                czy_16_02,
              data = uber)
-resettest(uber_m, power = 2:3, type = "fitted")
+resettest(uber_m2, power = 2:3, type = "fitted")
+# RESET nie przechodzi
 
-taxi_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
+taxi_m2 <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
                I(distance**2) +
                I(trip_time_mins**2) +
-               ride_count +
+               distance:trip_time_mins +
                hour_gr + 
                czy_weekend +
                hour_gr:czy_weekend +
@@ -260,95 +148,51 @@ taxi_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
                czy_19_01 +
                czy_16_02,
              data = taxi)
-resettest(taxi_m, power = 2:3, type = "fitted")
+resettest(taxi_m2, power = 2:3, type = "fitted")
+# RESET nie przechodzi
 
-stargazer(lyft_m, uber_m, taxi_m,
+stargazer(uber_m2, taxi_m2,
           type = "text", align = TRUE, style = "default", df = FALSE)
-# dodanie kwadratów i interakcji nic nie wnosi dla RESETU
+# dodanie kwadratów i interakcji nic nie wnosi dla RESETU, ale daje 
+#   ciekawe wyniki dotyczące nieliniowości, więc warto przeanalizować
+#   te rozszerzone modele
+
+# Współczynnik VIF
+vif(uber_m, type = "predictor")
+vif(taxi_m, type = "predictor")
+
+# Modele bazowe i rozszerzone
+stargazer(uber_m, uber_m2, taxi_m2, taxi_m,
+          type = "text", align = TRUE, style = "default", df = FALSE)
+
+# Usuwamy niepotrzebne już modele bazowe
+rm(uber_m, taxi_m)
 
 # Reszty
 
 # homoskedastyczność
-bptest(lyft_m)
-bptest(uber_m)
-bptest(taxi_m)
+bptest(uber_m2)
+bptest(taxi_m2)
+# występuje, więc trzeba będzie użyć macierzy odpornych
 
 # normalność
-jarque.bera.test(lyft_m$residuals)
-jarque.bera.test(uber_m$residuals)
-jarque.bera.test(taxi_m$residuals)
+jarque.bera.test(uber_m2$residuals)
+jarque.bera.test(taxi_m2$residuals)
+# występuje, ale próba jest duża, więc nie ma problemu
+
+
+# Macierze odporne
+
+uber_odp <- coeftest(uber_m2, vcov.=vcovHC(uber_m2, type="HC0"))
+taxi_odp <- coeftest(taxi_m2, vcov.=vcovHC(taxi_m2, type="HC0"))
+
+stargazer(uber_odp, taxi_odp,
+          type = "text", align = TRUE, style = "default", df = FALSE)
 
 
 save.image("workspace.RData")
 
 load("workspace.RData")
-
-
-
-# Macierze odporne
-
-lyft_odp <- coeftest(lyft_m, vcov.=vcovHC(lyft_m, type="HC0"))
-uber_odp <- coeftest(uber_m, vcov.=vcovHC(uber_m, type="HC0"))
-taxi_odp <- coeftest(taxi_m, vcov.=vcovHC(taxi_m, type="HC0"))
-
-stargazer(lyft_m, uber_m, taxi_m,
-          type = "text", align = TRUE, style = "default", df = FALSE)
-
-
-
-# MODELE DLA ride_count
-
-# Log - lin
-lyft_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-               ride_count,
-             data = lyft)
-resettest(lyft_m, power = 2:3, type = "fitted")
-
-uber_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-               ride_count,
-             data = uber)
-resettest(uber_m, power = 2:3, type = "fitted")
-
-taxi_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-               ride_count,
-             data = taxi)
-resettest(taxi_m, power = 2:3, type = "fitted")
-
-stargazer(lyft_m, uber_m, taxi_m,
-          type = "text", align = TRUE, style = "default", df = FALSE)
-
-# log - lin z kwadratmi
-lyft_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-               I(distance**2) +
-               I(trip_time_mins**2) +
-               ride_count,
-             data = lyft)
-resettest(lyft_m, power = 2:3, type = "fitted")
-
-uber_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-               I(distance**2) +
-               I(trip_time_mins**2) +
-               ride_count,
-             data = uber)
-resettest(uber_m, power = 2:3, type = "fitted")
-
-taxi_m <- lm(I(log(total_amount)) ~ distance + trip_time_mins +
-               I(distance**2) +
-               I(trip_time_mins**2) +
-               ride_count,
-             data = taxi)
-resettest(taxi_m, power = 2:3, type = "fitted")
-
-stargazer(lyft_m, uber_m, taxi_m,
-          type = "text", align = TRUE, style = "default", df = FALSE)
-
-
-
-
-
-
-
-
 
 
 
