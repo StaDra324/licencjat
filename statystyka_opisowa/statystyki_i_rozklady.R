a92b099d503ae0653740d1ed1e83d6a245f89faf
StaDra324 <s.drag@student.uw.edu.pl>
2026-03-08 18:35:36 +0100

    Czyszczenie kodów 3
    Częściowo wyczyszczony kod do statystyk - ułożony w odpowiedniej kolejności, z zaznaczeniem co najprawdopodobniej będzie użyte, ale do zatwierdzenia jeszcze, i inne rzeczy póki co przesunięte ale nie usunięte

--- statystyka_opisowa/statystyki_i_rozklady.R
+++ statystyka_opisowa/statystyki_i_rozklady.R
@@ -3,62 +3,20 @@ library(psych)
 library(skimr)
 library(patchwork)
 
-# WCZYTANIE I DOSTOSOWANIE DANYCH
-styczen = read.csv("wszystkie_do_statOp_styczen.csv")
-luty = read.csv("wszystkie_do_statOp_luty.csv")
-marzec = read.csv("wszystkie_do_statOp_marzec.csv")
+# WCZYTANIE DANYCH
+styczen = read_csv("wszystkie_do_statOp_styczen.csv")
+luty = read_csv("wszystkie_do_statOp_luty.csv")
+marzec = read_csv("wszystkie_do_statOp_marzec.csv")
 
 wszystkie <- rbind(styczen, luty, marzec)
 
-# STATYSTYKI OPISOWE
+# Usunięcie niepotrzebnych zbiorów
+rm(styczen, luty, marzec)
 
-# Funkcja do sprawdzania rozkładu do przedziałów
-przedzialy <- function(df) {
-  
-  distance <- c(
-    "< 0.15"  = sum(df$distance < 0.15),
-    "0.15–3" = sum(df$distance >= 0.15 & df$distance <= 3),
-    "3–15"  = sum(df$distance > 3 & df$distance <= 15),
-    "> 15"   = sum(df$distance > 15)
-  )
-  
-  trip_time_mins <- c(
-    "< 1"  = sum(df$trip_time_mins < 1),
-    "1–20" = sum(df$trip_time_mins >= 1 & df$trip_time_mins <= 20),
-    "20–60"  = sum(df$trip_time_mins > 20 & df$trip_time_mins <= 60),
-    "> 60"   = sum(df$trip_time_mins > 60)
-  )
-  
-  base_fare <- c(
-    "< 3"  = sum(df$base_fare < 3),
-    "3-25" = sum(df$base_fare >= 3 & df$base_fare <= 25),
-    "25–90"  = sum(df$base_fare > 25 & df$base_fare <= 90),
-    "> 90"   = sum(df$base_fare > 90)
-  )
-  
-  total_amount <- c(
-    "< 6"  = sum(df$total_amount < 6),
-    "6-30" = sum(df$total_amount >= 6 & df$total_amount <= 30),
-    "30-105"  = sum(df$total_amount > 30 & df$total_amount <= 105),
-    "> 105"   = sum(df$total_amount > 105)
-  )
-  
-  list(
-    distance = distance,
-    trip_time_mins = trip_time_mins,
-    base_fare = base_fare,
-    total_amount = total_amount
-  )
-}
-
-summary(wszystkie)
-describe(wszystkie)
-skim(wszystkie)
-przedzialy(wszystkie)
-#table(wszystkie$weekday)
-
+# STATYSTYKA OPISOWA
 
 # Wyrzucamy obserwacje poniżej kwantyla 0.01 i powyżej kwantyla 0.99
+
 p_lo <- 0.01
 p_hi <- 0.99
 
@@ -80,8 +38,11 @@ wszystkie_obciete <- wszystkie %>%
     between(distance, lo_dist, hi_dist),
     between(trip_time_mins, lo_time, hi_time),
     between(base_fare, lo_base, hi_base),
-    between(total_amount, lo_tot, hi_tot)
-  )
+    between(total_amount, lo_tot, hi_tot))
+
+# Usuwamy niepotrzebne
+rm(hi_base, hi_dist, hi_time, hi_tot, 
+   lo_base, lo_dist, lo_time, lo_tot, p_hi, p_lo)
 
 # ile zmiennych wyrzucono
 tibble(
@@ -92,20 +53,261 @@ tibble(
     (nrow(wszystkie) - nrow(wszystkie_obciete)) / nrow(wszystkie) * 100
 )
 
+# Statysyki opisowe
+
+options(scipen = 999)
+
+# oprócz kwartyli
+wszystkie %>%
+  select(distance, trip_time_mins, base_fare, total_amount) %>%
+  describeBy(group = wszystkie$provider, mat = TRUE) %>%
+  select(-c(item, vars)) %>%
+  mutate(across(where(is.numeric), round, 2))
+
+wszystkie_obciete %>%
+  select(distance, trip_time_mins, base_fare, total_amount) %>% 
+  describeBy(group = wszystkie_obciete$provider, mat = TRUE) %>%
+  select(-c(item, vars)) %>%
+  mutate(across(where(is.numeric), round, 2))
+
+# kwartyle
+#           ????????????
+#             ????????????
+
+# wszystkie %>%
+#   filter(provider == "Uber") %>%
+#   select(distance, trip_time_mins, base_fare, total_amount) %>%
+#   skim()
+# 
+# wszystkie_obciete %>%
+#   filter(provider == "Uber") %>%
+#   select(distance, trip_time_mins, base_fare, total_amount) %>%
+#   skim()
+# 
+# wszystkie %>%
+#   filter(provider == "Taxi") %>%
+#   select(distance, trip_time_mins, base_fare, total_amount) %>%
+#   skim()
+# 
+# wszystkie_obciete %>%
+#   filter(provider == "Taxi") %>%
+#   select(distance, trip_time_mins, base_fare, total_amount) %>%
+#   skim()
+
+# usunięcie po kwantylach pozytywnie wpłynęło na outliery, skośność i 
+#   kurtozę, a core rozkładów wydaje pozostał praktycznie nienaruszony
+
+
+# Histogramy dla czasu, dystansu i ceny
+
+# dystans
+ggplot(wszystkie_obciete, aes(x = distance)) +
+  geom_histogram(bins = 50, fill = "steelblue", color = "black") +
+  facet_wrap(~ provider) +
+  theme_minimal()
+
+# czas
+ggplot(wszystkie_obciete, aes(x = trip_time_mins)) +
+  geom_histogram(bins = 50, fill = "steelblue", color = "black") +
+  facet_wrap(~ provider) +
+  theme_minimal()
+
+# cena
+ggplot(wszystkie_obciete, aes(x = total_amount)) +
+  geom_histogram(bins = 50, fill = "steelblue", color = "black") +
+  facet_wrap(~ provider) +
+  theme_minimal()
+
+
+# Wykresy rozproszenia (wersja 2d density)
+w1 <- ggplot(wszystkie_obciete, aes(distance, total_amount)) +
+  stat_bin2d(bins = 400) +
+  facet_wrap(~provider)
+
+w2 <- ggplot(wszystkie_obciete, aes(trip_time_mins, total_amount)) +
+  stat_bin2d(bins = 400) +
+  facet_wrap(~provider)
+
+w3 <- ggplot(wszystkie_obciete, aes(distance, trip_time_mins)) +
+  stat_bin2d(bins = 400) +
+  facet_wrap(~provider)
+
+w1 / w2 / w3
+
+# Usuwamy niepotrzebne
+rm(w1, w2, w3)
+
+# Korelacje
+wszystkie_obciete %>%
+  group_by(provider) %>%
+  summarise(
+    cor_price_dist = cor(total_amount, distance, method = "spearman"),
+    cor_price_time = cor(total_amount, trip_time_mins, method = "spearman"),
+    cor_dist_time  = cor(distance, trip_time_mins, method = "spearman"))
+
+
+# Natężenie przejazdów
+
+# Heatmapy liczby przejazdów w dniach i godzinach
+
+hm_wd <- wszystkie_obciete %>%
+  count(provider, weekday, hour, name = "n")
+
+hm_wd$weekday <- factor(
+  hm_wd$weekday,
+  levels = 0:6,
+  labels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"))
+
+ggplot(hm_wd, aes(x = weekday, y = hour, fill = n)) +
+  geom_tile() +
+  facet_wrap(~ provider, ncol = 1) +
+  scale_fill_continuous(trans = "reverse") +
+  scale_y_continuous(breaks = 0:23) +
+  labs(x = "weekday", y = "hour", fill = "rides") +
+  theme_minimal()+
+  theme(axis.text.y = element_text(size = 5))
+
+# Histogram liczby przejazdów w godzinach w podziale na dni robocze i weekend
+
+# dodajemy zmienną czy_weekend
+wszystkie_obciete <- wszystkie_obciete %>%
+  mutate(czy_weekend = if_else(weekday >= 5, "Weekend", "Robocze"))
+
+ggplot(wszystkie_obciete, aes(x = hour)) +
+  geom_histogram(binwidth = 1, boundary = -0.5, fill = "steelblue", color = "black") +
+  facet_grid(czy_weekend ~ provider) +
+  scale_x_continuous(breaks = 0:23) +
+  theme_minimal()
+
+
+
+
+# Eksportujemy obciete zbiory do csv
+
+write.csv(filter(wszystkie_obciete, provider == "Taxi"),
+          "taxi_do_modelu.csv", row.names = FALSE)
+
+write.csv(filter(wszystkie_obciete, provider == "Uber"),
+          "uber_do_modelu.csv", row.names = FALSE)
+
+
+
+#
+# 
+# MOŻNA COŚ Z TEGO WZIĄĆ, ALE DLA UBERA NIE MA SCHEMATU...
+
+
+# Heatmapy mediany cen w podziale na daty
+
+hm_price <- wszystkie_obciete %>%
+  mutate(date = as.Date(datetime)) %>%
+  group_by(provider, month, date, hour) %>%
+  summarise(mean_price = mean(total_amount), .groups = "drop")
+
+# uber
+ggplot(hm_price %>% filter(provider == "Uber"), aes(x = date, y = hour, fill = mean_price)) +
+  geom_tile() +
+  facet_wrap(~ month, ncol = 1, scales = "free_x") +
+  scale_fill_continuous(trans = "reverse") +
+  scale_x_date(date_breaks = "1 day", labels = function(x) format(x, "%a")) +
+  scale_y_continuous(breaks = seq(1, 23, 2)) +
+  labs(fill = "Mean price") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        axis.text.y = element_text(size = 4))
+
+# taxi
+ggplot(hm_price %>% filter(provider == "Taxi"), aes(x = date, y = hour, fill = mean_price)) +
+  geom_tile() +
+  facet_wrap(~ month, ncol = 1, scales = "free_x") +
+  scale_fill_continuous(trans = "reverse") +
+  scale_x_date(date_breaks = "1 day", labels = function(x) format(x, "%a")) +
+  scale_y_continuous(breaks = 0:23) +
+  labs(fill = "Mean price") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        axis.text.y = element_text(size = 4))
+
+
+# Heatmapy mediany i średniej opłat w dniach i godzinach
+
+hm_mean <- wszystkie_obciete %>%
+  group_by(provider, weekday, hour) %>%
+  summarise(mean_total = mean(total_amount), .groups = "drop")
+
+hm_mean$weekday <- factor(
+  hm_mean$weekday,
+  levels = 0:6,
+  labels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"))
+
+ggplot(hm_mean, aes(x = weekday, y = hour, fill = mean_total)) +
+  geom_tile() +
+  facet_wrap(~ provider, ncol = 1) +
+  scale_fill_continuous(trans = "reverse", name = "mean\ntotal_amount") +
+  scale_y_continuous(breaks = 0:23) +
+  labs(x = "weekday", y = "hour") +
+  theme_minimal()
+
+#
+#
+#
+
+
+# RACZEJ DO POMINIĘCIA / POJEDYNCZE DO ANEKSU
+
+# !!!!!!!!!!!
+#   !!!!!!!!!!!!!!!!
+#   !!!!!!!!!!!!!!!!!!!!!!
+
+# Funkcja do sprawdzania rozkładu do przedziałów
+przedzialy <- function(df) {
+  
+  distance <- c(
+    "< 0.3"  = sum(df$distance < 0.3),
+    "0.3–3" = sum(df$distance >= 0.3 & df$distance <= 3),
+    "3–15"  = sum(df$distance > 3 & df$distance <= 15),
+    "> 15"   = sum(df$distance > 15)
+  )
+  
+  trip_time_mins <- c(
+    "< 3"  = sum(df$trip_time_mins < 3),
+    "3–20" = sum(df$trip_time_mins >= 3 & df$trip_time_mins <= 20),
+    "20–60"  = sum(df$trip_time_mins > 20 & df$trip_time_mins <= 60),
+    "> 60"   = sum(df$trip_time_mins > 60)
+  )
+  
+  base_fare <- c(
+    "< 3"  = sum(df$base_fare < 3),
+    "3-25" = sum(df$base_fare >= 3 & df$base_fare <= 25),
+    "25–50"  = sum(df$base_fare > 25 & df$base_fare <= 50),
+    "> 50"   = sum(df$base_fare > 50)
+  )
+  
+  total_amount <- c(
+    "< 6"  = sum(df$total_amount < 6),
+    "6-30" = sum(df$total_amount >= 6 & df$total_amount <= 30),
+    "30-70"  = sum(df$total_amount > 30 & df$total_amount <= 70),
+    "> 70"   = sum(df$total_amount > 70)
+  )
+  
+  list(
+    distance = distance,
+    trip_time_mins = trip_time_mins,
+    base_fare = base_fare,
+    total_amount = total_amount
+  )
+}
+
+summary(wszystkie)
+describe(wszystkie)
+skim(wszystkie)
+przedzialy(wszystkie)
+
 summary(wszystkie_obciete)
 describe(wszystkie_obciete)
 skim(wszystkie_obciete)
 przedzialy(wszystkie_obciete)
 
-# przed <- przedzialy(wszystkie)
-# po    <- przedzialy(wszystkie_obciete)
-# list(przed = przed, po = po)
-
-options(scipen = 999)
-desc1 <- describeBy(wszystkie, group = wszystkie$provider, mat = TRUE)
-desc1 %>% mutate(across(where(is.numeric), round, 2))
-desc2 <- describeBy(wszystkie_obciete, group = wszystkie_obciete$provider, mat = TRUE)
-desc2 %>% mutate(across(where(is.numeric), round, 2))
 
 skim(filter(wszystkie, provider == "Taxi"))
 skim(filter(wszystkie_obciete, provider == "Taxi"))
@@ -113,17 +315,11 @@ skim(filter(wszystkie_obciete, provider == "Taxi"))
 skim(filter(wszystkie, provider == "Uber"))
 skim(filter(wszystkie_obciete, provider == "Uber"))
 
-skim(filter(wszystkie, provider == "Lyft"))
-skim(filter(wszystkie_obciete, provider == "Lyft"))
-# usunięcie po kwantylach pozytywnie wpłynęło na outliery, skośność i 
-#   kurtozę, ale core rozkładów wydaje się że został
 
-
-# WIZUALIZACJA DANYCH
+# Porównanie danych przed i po obcięciu
 
 par(mfrow = c(1, 2))
 
-# Porównania zmiennych
 # porównanie total_amount
 boxplot(total_amount ~ provider, data = wszystkie,
         ylim = c(0, 120),
@@ -161,60 +357,20 @@ boxplot(trip_time_mins ~ provider, data = wszystkie_obciete,
         main = "Trip_time_mins po obcieciu")
 # ogólnie boxploty potwierdzają, że obcięcie dużo dało, rozkłady praktycznie 
 #   się nie zmieniły, a prawie wszystkie wartości teraz mają sens praktyczny
-
+  
 # Usuwamy niepotrzebne rzeczy z pamięci
-rm(desc1, desc2, wszystkie, hi_base, hi_dist, 
-   hi_time, hi_tot, lo_base, lo_dist, lo_time, lo_tot, p_hi, p_lo)
+rm(wszystkie)
+
 
 # Wykresy rozproszenia (wersja 2d density)
-w1 <- ggplot(wszystkie_obciete, aes(distance, total_amount)) +
-  stat_bin2d(bins = 400) +
-  facet_wrap(~provider)
-
-w2 <- ggplot(wszystkie_obciete, aes(trip_time_mins, total_amount)) +
-  stat_bin2d(bins = 400) +
-  facet_wrap(~provider)
-
-w1 / w2
-
-rm(w1, w2)
-
 ggplot(filter(wszystkie_obciete, provider == "Taxi"), aes(distance, base_fare)) +
   stat_bin2d(bins = 400) 
 
 ggplot(filter(wszystkie_obciete, provider == "Lyft"), aes(distance, base_fare)) +
   stat_bin2d(bins = 400) 
 
-ggplot(wszystkie_obciete, aes(distance, trip_time_mins)) +
-  stat_bin2d(bins = 400) +
-  facet_wrap(~provider)
-
-# Korelacje
-wszystkie_obciete %>%
-  group_by(provider) %>%
-    summarise(
-      cor_price_dist = cor(total_amount, distance, method = "spearman"),
-      cor_price_time = cor(total_amount, trip_time_mins, method = "spearman"),
-      cor_dist_time  = cor(distance, trip_time_mins, method = "spearman")
-    )
-
-# wszystkie_obciete %>%
-#   group_by(provider) %>%
-#   group_map(~ {
-#     cor(.x[, c("total_amount","distance","trip_time_mins")],
-#         method = "spearman")
-#   })
 
 # Rozkłady total_amount w godzinach
-# lyft w godzinach
-boxplot(total_amount ~ hour, 
-        data = wszystkie_obciete %>% filter(provider == "Lyft"),
-        main = "Lyft w godzinach")
-        
-boxplot(total_amount ~ hour, 
-        data = wszystkie_obciete %>% filter(provider == "Lyft"),
-        xlab = "Godzina", ylab = "Total amount",
-        ylim = c(0, 40))
 
 # uber w godzinach
 boxplot(total_amount ~ hour, 
@@ -250,7 +406,7 @@ ggplot() +
   geom_line(
     data = hour_total,
     aes(hour, median_price, color = provider),
-    size = 1
+    linewidth = 1
   ) +
   geom_point(
     data = hour_total,
@@ -262,7 +418,7 @@ ggplot() +
     data = hour_base,
     aes(hour, median_price, color = provider),
     linetype = "dashed",
-    size = 1
+    linewidth = 1
   ) +
   geom_point(
     data = hour_base,
@@ -278,15 +434,6 @@ ggplot() +
   )
 
 # Rozkłady w dniach
-# lyft w dniach
-boxplot(total_amount ~ weekday, 
-        data = wszystkie_obciete %>% filter(provider == "Lyft"),
-        main = "Lyft w dniach")
-
-boxplot(total_amount ~ weekday, 
-        data = wszystkie_obciete %>% filter(provider == "Lyft"),
-        xlab = "Dzień", ylab = "Total amount",
-        ylim = c(0, 40))
 
 # uber w dniach
 boxplot(total_amount ~ weekday, 
@@ -322,7 +469,7 @@ ggplot() +
   geom_line(
     data = weekday_total,
     aes(weekday, median_price, color = provider),
-    size = 1
+    linewidth = 1
   ) +
   geom_point(
     data = weekday_total,
@@ -334,7 +481,7 @@ ggplot() +
     data = weekday_base,
     aes(weekday, median_price, color = provider),
     linetype = "dashed",
-    size = 1
+    linewidth = 1
   ) +
   geom_point(
     data = weekday_base,
@@ -349,52 +496,30 @@ ggplot() +
     color = "Provider"
   )
 
+
+
 # NATĘŻENIE PRZEJAZDÓW
 
 # Heatmapy liczby przejazdów w podziale na daty
-
-# łącznie
-hm <- wszystkie_obciete %>%
-  mutate(date = as.Date(datetime)) %>%
-  count(month, date, hour, name = "n")
-
-ggplot(hm, aes(x = date, y = hour, fill = n)) +
-  geom_tile() +
-  facet_wrap(~ month, ncol = 1, scales = "free_x") +
-  scale_fill_continuous(trans = "reverse") +
-  scale_x_date(date_breaks = "1 day", labels = function(x) format(x, "%a")) +
-  scale_y_continuous(breaks = 0:23) +
-  labs(x = "date", y = "hour", fill = "rides") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 # po providerach
 hm <- wszystkie_obciete %>%
   mutate(date = as.Date(datetime)) %>%
   count(provider, month, date, hour, name = "n")
 
-# lyft
-ggplot(hm %>% filter(provider == "Lyft"), aes(date, hour, fill = n)) +
-  geom_tile() +
-  facet_wrap(~ month, ncol = 1, scales = "free_x") +
-  scale_fill_continuous(trans = "reverse") +
-  scale_x_date(date_breaks = "1 day", labels = function(x) format(x, "%a")) +
-  scale_y_continuous(breaks = 0:23) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-
 # uber
-ggplot(hm %>% filter(provider == "Uber"), aes(date, hour, fill = n)) +
+ggplot(hm %>% filter(provider == "Uber"), aes(x = date, y = hour, fill = n)) +
   geom_tile() +
   facet_wrap(~ month, ncol = 1, scales = "free_x") +
   scale_fill_continuous(trans = "reverse") +
   scale_x_date(date_breaks = "1 day", labels = function(x) format(x, "%a")) +
-  scale_y_continuous(breaks = 0:23) +
+  scale_y_continuous(breaks = seq(1, 23, 2)) +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        axis.text.y = element_text(size = 4))
 
 # taxi
-ggplot(hm %>% filter(provider == "Taxi"), aes(date, hour, fill = n)) +
+ggplot(hm %>% filter(provider == "Taxi"), aes(x = date, y = hour, fill = n)) +
   geom_tile() +
   facet_wrap(~ month, ncol = 1, scales = "free_x") +
   scale_fill_continuous(trans = "reverse") +
@@ -402,93 +527,16 @@ ggplot(hm %>% filter(provider == "Taxi"), aes(date, hour, fill = n)) +
   scale_y_continuous(breaks = 0:23) +
   #scale_y_continuous(breaks = seq(1, 23, 2))        alternatywne etykiety na y
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+        axis.text.y = element_text(size = 4))
 
-# Heatmapy liczby przejazdów w dniach i godzinach
-
-hm_wd <- wszystkie_obciete %>%
-  count(provider, weekday, hour, name = "n")
-
-ggplot(hm_wd, aes(x = weekday, y = hour, fill = n)) +
-  geom_tile() +
-  facet_wrap(~ provider, ncol = 1) +
-  scale_fill_continuous(trans = "reverse") +
-  scale_y_continuous(breaks = 0:23) +
-  labs(x = "weekday", y = "hour", fill = "rides") +
-  theme_minimal()
-
-# Histogram liczby przejazdów w godzinach w podziale na dni robocze i weekend
-
-wszystkie_obciete <- wszystkie_obciete %>%
-  mutate(czy_weekend = if_else(weekday >= 5, "Weekend", "Robocze"))
-
-ggplot(wszystkie_obciete, aes(x = hour)) +
-  geom_histogram(binwidth = 1, boundary = -0.5, fill = "steelblue", color = "black") +
-  facet_grid(czy_weekend ~ provider) +
-  scale_x_continuous(breaks = seq(1, 23, 2)) +
-  theme_minimal()
-
-  
-# Heatmapy mediany i średniej opłat w dniach i godzinach
-
-hm_med <- wszystkie_obciete %>%
-  group_by(provider, weekday, hour) %>%
-  summarise(median_total = median(total_amount), .groups = "drop")
-
-ggplot(hm_med, aes(x = weekday, y = hour, fill = median_total)) +
-  geom_tile() +
-  facet_wrap(~ provider, ncol = 1) +
-  scale_fill_continuous(trans = "reverse", name = "median\ntotal_amount") +
-  scale_y_continuous(breaks = 0:23) +
-  labs(x = "weekday", y = "hour") +
-  theme_minimal()
-
-hm_mean <- wszystkie_obciete %>%
-  group_by(provider, weekday, hour) %>%
-  summarise(mean_total = mean(total_amount), .groups = "drop")
-
-ggplot(hm_mean, aes(x = weekday, y = hour, fill = mean_total)) +
-  geom_tile() +
-  facet_wrap(~ provider, ncol = 1) +
-  scale_fill_continuous(trans = "reverse", name = "mean\ntotal_amount") +
-  scale_y_continuous(breaks = 0:23) +
-  labs(x = "weekday", y = "hour") +
-  theme_minimal()
+# !!!!!!!!!!!!!!!!!
+#   !!!!!!!!!!!!!!!!!!
+#   !!!!!!!!!!!!!!!!!!!!!!!
 
 
-# Histogramy dla czasu, dystansu i ceny
-
-# dystans
-ggplot(wszystkie_obciete, aes(x = distance)) +
-  geom_histogram(bins = 50, fill = "steelblue", color = "black") +
-  facet_wrap(~ provider) +
-  theme_minimal()
-
-# czas
-ggplot(wszystkie_obciete, aes(x = trip_time_mins)) +
-  geom_histogram(bins = 50, fill = "steelblue", color = "black") +
-  facet_wrap(~ provider) +
-  theme_minimal()
-
-# cena
-ggplot(wszystkie_obciete, aes(x = total_amount)) +
-  geom_histogram(bins = 50, fill = "steelblue", color = "black") +
-  facet_wrap(~ provider) +
-  theme_minimal()
 
 
-# Eksportujemy obciete zbiory do csv
-#write.csv(wszystkie_obciete,
-#          "wszystkie_do_modelu.csv", row.names = FALSE)
-
-write.csv(filter(wszystkie_obciete, provider == "Taxi"),
-          "taxi_do_modelu.csv", row.names = FALSE)
-
-write.csv(filter(wszystkie_obciete, provider == "Uber"),
-          "uber_do_modelu.csv", row.names = FALSE)
-
-write.csv(filter(wszystkie_obciete, provider == "Lyft"),
-          "lyft_do_modelu.csv", row.names = FALSE)
 
 
 
